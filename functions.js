07248f6b0612e2b1e0e3fdc3b810d05e555f71c7
Smokeybro916 <kyleluangasa@gmail.com>
2023-08-31 20:03:10 -0400

    first commit

---
+++ functions.js
@@ -0,0 +1,8 @@
+
+function rant(message){
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+
+/*From Udemy the example was to have a argument to make the text uppercase on what you hate */
