5fdce92ed91cc1d7cd98719f34fbb69b6d809933
Smokeybro916 <kyleluangasa@gmail.com>
2023-09-06 19:53:58 -0400

    third commit

--- functions.js
+++ functions.js
@@ -1,15 +1,27 @@
-
+/*
 function rant(message){
     console.log(message.toUpperCase());
     console.log(message.toUpperCase());
     console.log(message.toUpperCase());
 }
 
-/*From Udemy the example was to have a argument to make the text uppercase on what you hate */
+From Udemy the example was to have a argument to make the text uppercase on what you hate */
 
-
+/*
 const square = function (num) {
     return num * num;
 }
 
-/*example of a Function Expression. A Function expression is that the function is stored inside a variable. */
+example of a Function Expression. A Function expression is that the function is stored inside a variable. */
+
+/*
+const square = {
+    area: function (side){
+        return side * side;
+    },
+    perimeter: function(side){
+        return side * 4;
+    }
+}
+
+example of a methods */
