8c18534c817eef7f7c9fb8ac66ca1cd96930420a
Smokeybro916 <kyleluangasa@gmail.com>
2023-09-07 19:50:41 -0400

    fifth commit

--- functions.js
+++ functions.js
@@ -36,3 +36,12 @@ const hen = {
     }
 }
 An example of using "this" */
+
+/*
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+const firstNames = fullNames.map(function(name){
+    return name.first;
+});
+
+ Using the .map to create an array with only the first names*/
